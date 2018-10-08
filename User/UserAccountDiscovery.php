f89180213b11be82c5ab0ab1ef91f317ad6d8903
Spomky <florent@morselli.fr>
2018-10-08 13:23:31 +0200

    WIP

--- User/UserAccountDiscovery.php
+++ User/UserAccountDiscovery.php
@@ -13,12 +13,9 @@ declare(strict_types=1);
 
 namespace OAuth2Framework\Component\AuthorizationEndpoint\User;
 
-use OAuth2Framework\Component\Core\User\User;
 use OAuth2Framework\Component\Core\UserAccount\UserAccount;
 
-interface UserDiscovery
+interface UserAccountDiscovery
 {
-    public function getCurrentUser(): ?User;
-
     public function getCurrentAccount(): ?UserAccount;
 }
