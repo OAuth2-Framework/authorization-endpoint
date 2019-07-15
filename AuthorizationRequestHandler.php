b4c8a354a5ca2d0851e84ba21789dcfe5a4e72fe
Spomky <florent@morselli.fr>
2019-07-15 22:36:21 +0200

    Tests fixed

--- AuthorizationRequestHandler.php
+++ AuthorizationRequestHandler.php
@@ -16,7 +16,7 @@ namespace OAuth2Framework\Component\AuthorizationEndpoint;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-interface SelectAccountHandler
+interface AuthorizationRequestHandler
 {
     public function handle(ServerRequestInterface $request, string $authorizationId): ?ResponseInterface;
 }
